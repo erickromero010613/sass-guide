feba3fe0e2c3f03c575514026ed696ea5c561a21
Omar Villafuerte <ovillafuerte.94@gmail.com>
2021-04-18 16:39:38 -0400

    removed style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,12 +12,7 @@ module.exports = {
         rules: [
             {
                 test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader",
-                ],
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             },
         ],
     },
